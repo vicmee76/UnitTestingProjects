fdfe8ee409833a6d5f8571ec363b22f9663fe338
ComputerGuy <vicmee76@hotmail.com>
2022-03-05 20:27:30 +0100

    -- added class file for testing

--- FileProcess.cs
+++ FileProcess.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.IO;
 
 namespace UnitTestingProjects
 {
     public class FileProcess
     {
 
-
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("filename");
+            }
+            return File.Exists(fileName);
+        }
     }
 }
